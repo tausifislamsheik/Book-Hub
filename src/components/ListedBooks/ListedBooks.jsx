c3aea9f689d43fe82b7061e5c8946946b314e215
Tausif Islam Sheik <mr.tausif904@gmail.com>
2025-08-13 23:58:27 +0600

    Sort by button added in the listed books section

--- src/components/ListedBooks/ListedBooks.jsx
+++ src/components/ListedBooks/ListedBooks.jsx
@@ -18,6 +18,15 @@ const ListedBooks = () => {
     }, [])
     return (
         <div>
+            <div className='text-center my-10'>
+                 <div className="dropdown dropdown-center">
+                    <div tabIndex={0} role="button" className="btn m-1 bg-[#23BE0A] text-white px-5">Sort By</div>
+                    <ul tabIndex={0} className="dropdown-content menu bg-base-300 rounded-box z-1 w-52 p-2 shadow-sm">
+                        <li><a>Item 1</a></li>
+                        <li><a>Item 2</a></li>
+                    </ul>
+                 </div>
+            </div>
             <Tabs>
                 <TabList>
                 <Tab>Read Books</Tab>
