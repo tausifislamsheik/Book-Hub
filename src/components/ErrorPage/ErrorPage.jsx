89d532f82a2af08f0d2aff6ba2eca8291aacd00f
Tausif Islam Sheik <mr.tausif904@gmail.com>
2025-08-15 00:06:52 +0600

    Error page edited

--- src/components/ErrorPage/ErrorPage.jsx
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -3,7 +3,7 @@ import errorImg from '../../assets/error.jpg'
 const ErrorPage = () => {
     return (
         <div>
-            
+            <img className='w-4/6 mx-auto my-16 rounded-2xl' src={errorImg} alt="" />
         </div>
     );
 };
